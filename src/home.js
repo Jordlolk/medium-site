6fc55f7cc42b688e3b0a525082104d6f3cdb2c1e
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2024-01-04 20:14:36 -0300

    New mobile button test

--- src/home.js
+++ src/home.js
@@ -1,9 +1,43 @@
 const profilePic = document.getElementById('profilePic'),
 profileName = document.getElementById('profileName'),
-profileEmail = document.getElementById('profileEmail')
-console.log(profileEmail , profileName , profilePic);
+profileEmail = document.getElementById('profileEmail'),
+profilePagePc = document.getElementById('profilePagePc')
 let userLoggedIn = JSON.parse(localStorage.getItem('userLoggingIn'))
 console.log(userLoggedIn);
+
+/* const loadProfilePage = (e) => {
+    window.location.href = ''
+}
+profilePage.addEventListener('click' , loadProfilePage)
+ */
+
 /* profilePic.src = `${userLoggedIn.profile}`
 profileName.innerHTML = userLoggedIn.username
 profileEmail.innerHTML = userLoggedIn.email */
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Criar botão de menu
+  const menuButton = document.createElement("button");
+  menuButton.innerHTML = '&#9776'
+  menuButton.classList.add("buttonMobile");
+
+  // Adicionar botão ao DOM
+  const ul = document.querySelector("ul");
+  ul.appendChild(menuButton);
+
+  // Função para verificar a largura da tela e ajustar a visibilidade
+  function adjustMenuForScreenWidth(){
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 699) {
+      profilePagePc.style.display = "none";
+      menuButton.style.display = "block";
+    } else {
+      profilePagePc.style.display = "block";
+      menuButton.style.display = "none";
+    }
+  }
+
+  // Adicionar ouvintes de eventos
+  window.addEventListener("resize", adjustMenuForScreenWidth);
+  adjustMenuForScreenWidth(); // Chama a função inicialmente
+});
